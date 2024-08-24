1ddb6acf2d38ef08739d7a991780d5802e6b6bb7
ismoilnuriddinov <umidafayziyeva89@gmail.com>
2024-08-24 13:34:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+let ism = prompt("ismingizni kiriting");
+let familiya = prompt("bironta harfni  kiriting");
+;
+
+console.log("ism:" + "ism");
+console.log("familiya:" + "familiya");
+'k harf mavjud'
+
+
+
+// let ism = "Avazbek va Rashodbek";
+
+// console.log(ism);
+// console.log(ism.length);
+// console.log(ism.trim());
+// console.log(ism.slice(0, 8));
+// console.log(ism.includes("Ismoil"));
+// console.log(ism.indexOf("r"));
+// console.log(ism1.toLowerCase());
+// console.log(ism.toUpperCase());
+
+
+
